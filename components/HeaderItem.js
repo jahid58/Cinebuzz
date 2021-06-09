cf0c3da8f2c888510716cd4a21228001ecaeae15
Jahid Hasan <jahidjjaa@gmail.com>
2021-06-09 17:53:32 +0600

    second commit

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,6 +1,6 @@
 function HeaderItem({ Icon, title }) {
   return (
-    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 w-12 sm:20 hover:text-white">
+    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 w-12 sm:20 ">
       <Icon className="h-8 mb-1 group-hover:animate-bounce" />
       <p ClassName="opacity-100 group-hover:opacity-100 tracking-widest">
         {title}
